38a6755b79977d0e89c90995337b53814325b9d8
Jyoti Sheoran <sheoranjs24@users.noreply.github.com>
2020-12-08 12:02:54 -0800

    Update initial-storage-setup.sh

--- setup/initial-storage-setup.sh
+++ setup/initial-storage-setup.sh
@@ -1,48 +1,26 @@
 #!/bin/bash
 ####################################################
-# run as: sudo ./storage-setup.sh <username> [<drive1> <drive2>]
+# run as: sudo ./storage-setup.sh <username> <drive> <mount>
+# example: sudo ./storage-setup.sh sheoran /dev/sda /mnt/media
 # device names can be retrieved by running: fdisk -l
 # pre-requisite: format drives to ext4
 ####################################################
 
 ## Parameters
-if [[ ($# -eq 1 || $# -eq 3) ]];
+if [[ $# -eq 3 ]];
 then
   export USERNAME=$1
-  if [[ $# -eq 3 ]];
-  then
-    export DRIVE1=$2
-    export DRIVE2=$3
-  else
-    export DRIVE1=/dev/sda
-    export DRIVE2=/dev/sdb
-  fi
+  export DRIVE=$2
+  export DIRECTORY=$3  
 else
-  echo "Illegal number of parameters! run as:  sudo ./storage-setup.sh <username> [<drive1> <drive2>]"
+  echo "Illegal number of parameters! run as:  sudo ./storage-setup.sh <username> <drive> <mount>"
   exit(1)
 fi
 
-##-----------------
-## Format Drive
-##-----------------
-echo Do you wish to format the primary storage disk (y/n)?
-read input1
-if [[ $input1 == "y" ]]; then
-  parted --script $DRIVE1 \
-      mklabel gpt \
-      mkpart primary ext4 0GB 100%
-fi
-parted --script $DRIVE1 print
-
-echo Do you wish to format the secondary storage disk (y/n)?
-read input2
-if [[ $input1 == "y" ]]; then
-  parted --script $DRIVE2 \
-      mklabel gpt \
-      mkpart primary ext4 0GB 100%
-fi
-parted --script $DRIVE2 print
-# mkfs.ext4 /dev/sda1
+##----------------
+## List Devices
+##----------------
+sudo lsblk -o UUID,NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL,MODEL
 
 ##--------------------
 ## Install Dependencies
@@ -53,25 +31,30 @@ apt-get -qq install -y ntfs-3g                    # NTFS support
 apt-get -qq install -y exfat-utils                # exFAT support
 apt-get -qq install -y rsync
 
+##-----------------
+## Format Drive
+##-----------------
+echo "Do you wish to format the primary storage disk (y/n)?"
+read input1
+if [[ $input1 == "y" ]]; then
+  parted --script $DRIVE \
+      mklabel gpt \
+      mkpart primary ext4 0GB 100%
+fi
+parted --script $DRIVE print
+# mkfs.ext4 /dev/sda1
+
 ##--------------------
 ## Create Mounts
 ##--------------------
 export GROUPNAME=users
-export DRIVE1_UUID=`blkid -o value -s UUID $DRIVE1`
-mkdir /mnt/jsmedia1
-mount -t ext4 $DRIVE1 /mnt/jsmedia1
-chown -R $USERNAME:$GROUPNAME /mnt/jsmedia1
-chmod -R 750 /mnt/jsmedia1
-chmod -R g+s /mnt/jsmedia1
-echo "UUID=$DRIVE1_UUID\t/mnt/jsmedia1\text4\trw,auto,noatime,nofail\t0\t2" >> /etc/fstab
-
-export DRIVE2_UUID=`blkid -o value -s UUID $DRIVE2`
-mkdir /mnt/jsmedia2
-mount -t ext4 $DRIVE2 /mnt/jsmedia2
-chown -R $USERNAME:$GROUPNAME /mnt/jsmedia2
-chmod -R 750 /mnt/jsmedia2
-chmod -R g+s /mnt/jsmedia2
-echo "UUID=$DRIVE2_UUID\t/mnt/jsmedia2\text4\trw,auto,noatime,nofail\t0\t2" >> /etc/fstab
+export DRIVE_UUID=`blkid -o value -s UUID $DRIVE`
+mkdir /mnt/$DIRECTORY
+mount -t ext4 $DRIVE /mnt/DIRECTORY
+chown -R $USERNAME:$GROUPNAME /mnt/$DIRECTORY
+chmod -R 750 /mnt/$DIRECTORY
+chmod -R g+s /mnt/$DIRECTORY
+echo "UUID=$DRIVE_UUID\t/mnt/$DIRECTORY\text4\trw,auto,noatime,nofail\t0\t2" >> /etc/fstab
 
 ##--------------------
 ## Reboot
